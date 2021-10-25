45034ab2af7e4bc3c27aa4d319dcd8679ef09682
YuliiaHome <usik56@gmail.com>
2021-10-26 00:53:47 +0300

    26.10 home

--- page.js
+++ page.js
@@ -1,12 +1,24 @@
-const registrationBtn = document.querySelector('#registration')
-const popupRegistartion = document.querySelector('.reg__popup')
-const crossBtn = document.querySelector('#cross')
+const registrationBtn = document.querySelector("#registration");
+const popupRegistartion = document.querySelector(".reg__popup");
+const crossBtn = document.querySelector("#cross");
+// const overlay = document.querySelector("#overlay");
 
-registrationBtn.addEventListener('click', () => {
-    popupRegistartion.classList.add('reg__popup--show')
-})
+registrationBtn.addEventListener("click", () => {
+  popupRegistartion.classList.add("reg__popup--show");
+});
 
-crossBtn.addEventListener('click', () => {
-    popupRegistartion.classList.remove('reg__popup--show')
-})
+crossBtn.addEventListener("click", () => {
+  popupRegistartion.classList.remove("reg__popup--show");
+});
 
+// registrationBtn.addEventListener("click", () => {
+//   overlay.classList.add("#overlay");
+// });
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
