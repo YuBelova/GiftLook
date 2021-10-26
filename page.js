09f72a20075ada20e37a03538ddd393e6e699a6e
Yullia B <usik56@gmail.com>
2021-10-26 19:07:49 +0300

    office 26.10

--- page.js
+++ page.js
@@ -1,24 +1,24 @@
-const registrationBtn = document.querySelector("#registration");
-const popupRegistartion = document.querySelector(".reg__popup");
-const crossBtn = document.querySelector("#cross");
-// const overlay = document.querySelector("#overlay");
+const registrationBtn = document.querySelector('#registration')
+const popupRegistartion = document.querySelector('.reg__popup')
+const crossBtn = document.querySelector('#cross')
+const body = document.querySelector('main')
+const loginBtn = document.querySelector('#login')
+const popupLogin = document.querySelector('.login__popup')
+const overlay = document.querySelector(".overlay");
 
-registrationBtn.addEventListener("click", () => {
-  popupRegistartion.classList.add("reg__popup--show");
+registrationBtn.addEventListener('click', () => {
+    popupRegistartion.classList.add('reg__popup--show')
+    overlay.classList.add('overlay--show')
 });
-
-crossBtn.addEventListener("click", () => {
-  popupRegistartion.classList.remove("reg__popup--show");
+crossBtn.addEventListener('click', () => {
+    popupRegistartion.classList.remove('reg__popup--show')
 });
-
-// registrationBtn.addEventListener("click", () => {
-//   overlay.classList.add("#overlay");
-// });
-
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
-
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+loginBtn.addEventListener('click', () => {
+    overlay.classList.add('overlay--show')
+    popupLogin.classList.add('login__popup--show')
+})
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('overlay--show')
+    popupLogin.classList.remove('login__popup--show')
+    popupRegistartion.classList.remove('reg__popup--show');
+})
